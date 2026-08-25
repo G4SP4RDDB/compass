002e7040d9d8646ae34e0234eb1c1344b0c834f8
Gaspard Deltour Bauregard <gaspard.deltour.bauregard2@gmail.com>
2026-08-25 17:02:08 +0200

    init

---
+++ main.py
@@ -0,0 +1,7 @@
+def main():
+    print("HelloWorld")
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ stables.py
@@ -0,0 +1,6 @@
+from enum import Enum,auto
+
+
+class Stables(Enum):
+    USDC = auto()
+    USDT = auto()
